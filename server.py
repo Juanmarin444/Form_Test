0daf2ace3a9152d30b6ea1714b7b2f9b1265d757
Juan Marin <juanmarin@Work-MacBook-Pro.local>
2019-05-18 21:12:51 -0700

    Flask Work 1

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+# this route will handle our form submission
+# notice how we defined which HTTP methods are allowed by this route
+
+@app.route('/users', methods=['POST'])
+def create_user():
+   print("Got Post Info")
+   print(request.form)
+   # we'll talk about the following two lines after we learn a little more
+   # about forms
+   name = request.form['name']
+   email = request.form['email']
+   print(name, email)
+   # redirects back to the '/' route
+   return render_template('/success.html')
+
+@app.route('/users/<username>')
+def show_user_profile(username):
+  print(username)
+  return render_template('user.html', name=username)
+
+app.run(debug=True) # run our server
